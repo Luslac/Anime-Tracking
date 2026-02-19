5d072fd156e0b4cafe53478da7fa91dd5c8eba7f
Luslac <gabutmantap40@gmail.com>
2026-02-19 22:06:54 +0700

    final tulunk

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,6 +2,6 @@ import { web } from "./application/app.js";
 import { logger } from "./application/logging.js";
 
 const PORT = process.env.PORT || 3000;
-web.listen(PORT, () => {
+web.listen(PORT, '0.0.0.0', () => {
     logger.info(`Server Connected To Port ${PORT}`)
 })
