327ba909a26ff68843ba18603afc1fc452b3152b
Luslac <gabutmantap40@gmail.com>
2026-02-19 22:10:30 +0700

    last one god

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,7 +1,7 @@
 import { web } from "./application/app.js";
 import { logger } from "./application/logging.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 web.listen(PORT, '0.0.0.0', () => {
     logger.info(`Server Connected To Port ${PORT}`)
 })
